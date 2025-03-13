d77cef7a507fcb093e96d93219b4b55242ef6fb2
reyansh09 <reyansh.7814@gmail.com>
2025-03-13 07:29:02 +0530

    correct indentation

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -14,27 +14,27 @@ class WeatherController extends Controller
     }
     
     public function fetchWeather(Request $request)
-{
-    $city = $request->input('city', 'Delhi'); // Default city
-    $apiKey = env('OPENWEATHER_API_KEY');
-    $url = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric";
+    {
+        $city = $request->input('city', 'Delhi'); // Default city
+        $apiKey = env('OPENWEATHER_API_KEY');
+        $url = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric";
 
-    $response = Http::get($url);
-    $weather = $response->json();
-    // dd($weather);
+        $response = Http::get($url);
+        $weather = $response->json();
+        // dd($weather);
 
-    return redirect()->route('weatherDetail')->with([
-        'weather' => $weather,
-        'city' => $city
-    ]);
-}
-    
-public function weatherDetail()
-{
-    $weather = session('weather');
-    $city = session('city');
+        return redirect()->route('weatherDetail')->with([
+            'weather' => $weather,
+            'city' => $city
+        ]);
+    }
+        
+    public function weatherDetail()
+    {
+        $weather = session('weather');
+        $city = session('city');
 
-    return view('weatherdetail', compact('weather', 'city'));
-}
+        return view('weatherdetail', compact('weather', 'city'));
+    }
     
 }
